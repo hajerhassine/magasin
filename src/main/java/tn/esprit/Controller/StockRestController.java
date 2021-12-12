a168956e610cd5b7166cd92e20bebf1bf02ae9d9
ahmedchoko <ahmed.chokri@esprit.tn>
2021-12-12 10:49:18 +0100

    final stock

--- src/main/java/tn/esprit/Controller/StockRestController.java
+++ src/main/java/tn/esprit/Controller/StockRestController.java
@@ -1,5 +1,54 @@
 package tn.esprit.Controller;
 
-public class StockRestController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.Gson;
+
+import tn.esprit.entities.Stock;
+import tn.esprit.service.StockService;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/stock")
+public class StockRestController {
+	@Autowired
+	StockService stockservice;
+	@GetMapping("/retrievestockquantitybyproduct_category")
+	public List retrievestockquantitybyproduct_category() {
+		List result = stockservice.retrievestockquantitybyproduct_category();
+		return result;
+		}
+	@GetMapping("/retrieveproductquantitybyproduct_category_bystock")
+	public List retrieveproductquantitybyproduct_category_bystock() {
+		List result = stockservice.retrieveproductquantitybyproduct_category_bystock();
+		return result;
+		}
+	@GetMapping("/Retrieveallstock")
+	public List <Stock> Retrieveallstock() {
+		return stockservice.retrieveAllStocks();
+		}
+	
+	
+	 @PutMapping("/assignproduittostock/{idproduit}/{idstock}")
+	@ResponseBody
+	public void Assignproduittostock(@PathVariable("idproduit") Long idproduit,@PathVariable("idstock") Long idstock ) {
+		stockservice.assignProduittostock (idproduit,idstock);
+		}
+	 
+	 
+	 @GetMapping("/getalarm")
+		public String getalarm() {
+		 String s=stockservice.retrieveStatusStock();
+		 Gson gson = new Gson();    
+			return  gson.toJson(s);
+			}
 }
--- src/main/java/tn/esprit/service/StockServiceImpl.java
+++ src/main/java/tn/esprit/service/StockServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import tn.esprit.entities.Produit;
 import tn.esprit.entities.Stock;
 import tn.esprit.repository.Stockrepository;
 @Service
@@ -34,6 +35,51 @@ public class StockServiceImpl implements StockService{
 	public Stock retrieveStock(Long id) {
 		Stock t = stockrepository.findById(id).get();
 		return t;
+	}
+
+	@Override
+	public void assignProduittostock(Long idproduit, Long idstock) {
+		stockrepository.assignproduittostock(idproduit, idstock);
+		
+	}
+	//@Scheduled(cron="*/60 * * * * *")
+	@Override
+	public String retrieveStatusStock() {
+		List <Produit> produits=stockrepository.StatusStock();
+		String list="les produits dont la quantité de stock est epuisée: \n";
+		if(produits.isEmpty()){
+			list="Pas de produits dont la quantite de stock est epuiseé";
+		}
+		else{
+			for (Produit p : produits){
+			list=list+p.getLibelleproduit().toString()+" \n";
+		}
+		}
+		return list;
+	}
+
+	@Override
+	public void updatestockquantity(Long idstock) {
+		stockrepository.updatestockquantity(idstock);
+		
+	}
+
+	@Override
+	public List retrievestockquantitybyproduct_category() {
+	
+		return stockrepository.retrievestockquantitybyproduct_category();
+	}
+
+	@Override
+	public List retrieveproductquantitybyproduct_category_bystock() {
+
+		return stockrepository.retrieveproductquantitybyproduct_category_bystock();
+	}
+
+	@Override
+	public void delete(Long id) {
+		stockrepository.deleteById(id);
+		
 	}
 
 }
