adc1b5a5b83b1886cde9f6fbfe0f9462256a5a5f
ahmedchoko <ahmed.chokri@esprit.tn>
2021-12-12 11:22:37 +0100

    finalstock

--- src/main/java/tn/esprit/repository/Stockrepository.java
+++ src/main/java/tn/esprit/repository/Stockrepository.java
@@ -4,10 +4,39 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import tn.esprit.entities.Stock;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import tn.esprit.entities.Produit;
+import tn.esprit.entities.Stock;
 @Repository
 public interface Stockrepository extends CrudRepository<Stock,Long> {
 
-	
+	@Transactional
+	@Modifying
+	@Query("Update Produit p set p.stock.idstock = :idstock where p.idproduit =:idproduit")
+    void assignproduittostock(@Param("idproduit") Long idproduit, @Param("idstock") Long idstock);
+
+    @Query("SELECT count(*),p.detail.categorieproduit,s.libellestock FROM Produit p JOIN Stock s ON p.stock=s.idstock GROUP BY p.detail.categorieproduit, s.libellestock")
+	List retrieveproductquantitybyproduct_category_bystock();
+    
+    @Query("SELECT count(*),p.detail.categorieproduit,s.libellestock FROM Produit p JOIN Stock s ON p.stock=s.idstock GROUP BY p.detail.categorieproduit")
+   	List retrievestockquantitybyproduct_category();
+    
+    @Transactional
+	@Modifying
+	@Query("Update Stock s set s.quantite =s.quantite+1  where s.idstock =:idstock")
+    void updatestockquantity( @Param("idstock") Long idstock);
+    
+    
+    @Query("SELECT p FROM Produit p where p.stock.quanite_min>p.stock.quantite")
+    List<Produit> StatusStock();
 	
 	
 }
--- src/main/java/tn/esprit/service/StockService.java
+++ src/main/java/tn/esprit/service/StockService.java
@@ -6,10 +6,13 @@ import tn.esprit.entities.Stock;
 
 public interface StockService {
 	List<Stock> retrieveAllStocks();
-
 	Stock addStock(Stock s);
-
 	Stock updateStock(Stock u);
-
+   void delete(Long id);
 	Stock retrieveStock(Long id);
+	void assignProduittostock(Long idproduit,Long idstock);
+	String retrieveStatusStock();
+	void updatestockquantity(Long idstock);
+	List retrievestockquantitybyproduct_category();
+	List retrieveproductquantitybyproduct_category_bystock();
 }
